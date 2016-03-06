b9d448a25084106a5c8891ec73d93562bc4f7132
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-03-06 08:44:04 -0600

    Adding filters and Angular-Input-Masks

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -1,4 +1,4 @@
-angular.module('ioncurrency', ['ionic', 'ioncurrency.controllers', 'ioncurrency.services', 'ioncurrency.factory', 'ngCordova']);
+angular.module('ioncurrency', ['ionic', 'ioncurrency.controllers', 'ioncurrency.services', 'ioncurrency.factory', 'ngCordova', 'ui.utils.masks']);
 angular.module('ioncurrency.controllers', ['ioncurrency.services', 'ioncurrency.factory']);
 angular.module('ioncurrency.services', ['ioncurrency.factory']);
 angular.module('ioncurrency.factory', []);
